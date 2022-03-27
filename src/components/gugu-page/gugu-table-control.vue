52a4e6ca58dcd8c070b6991e101a4f458469e130
donghuitiao <326633131@qq.com>
2022-03-27 19:44:09 +0800

    style: 调整 control 面板样式

--- src/components/gugu-page/gugu-table-control.vue
+++ src/components/gugu-page/gugu-table-control.vue
@@ -6,26 +6,19 @@
 		</div>
 		<div class="control">
 			<el-divider content-position="left">排序</el-divider>
-			<div><el-radio v-model="sortType" label="" size="large">不排序</el-radio></div>
-			<div>
-				<el-radio v-model="sortType" label="currentGuguLength" size="large">根据当前咕咕时间</el-radio>
-			</div>
-			<div>
-				<el-radio v-model="sortType" label="averageGuguLength" size="large">根据平均咕咕时间</el-radio>
-			</div>
-			<div>
-				<el-radio v-model="sortType" label="maxGuguLength" size="large">根据最大咕咕时间</el-radio>
-			</div>
-			<div><el-radio v-model="sortType" label="videoNum" size="large">根据视频数量</el-radio></div>
+			<el-select v-model="sortType" class="m-2" placeholder="选择排序方式">
+				<el-option v-for="item in sortTypeOptions" :key="item.value" :label="item.label" :value="item.value" />
+			</el-select>
 
 			<el-divider content-position="left">是否降序排序</el-divider>
-			<el-switch v-model="sortOrder" />
+			<el-radio v-model="sortOrder" :label="false" size="large">升序</el-radio>
+			<el-radio v-model="sortOrder" :label="true" size="large">降序</el-radio>
 
 			<el-divider content-position="left">是否加入自己的数据</el-divider>
 			<el-switch v-model="isAddSelf" />
 
 			<el-divider content-position="left">搜索 up 主</el-divider>
-			<el-input v-model="userNameFilter" size="mini"></el-input>
+			<el-input v-model="userNameFilter"></el-input>
 		</div>
 	</div>
 </template>
@@ -33,6 +26,28 @@
 <script setup>
 import { useGugu } from '../../utils/useGugu';
 const { sortType, isAddSelf, sortOrder, userNameFilter, isShowControlDrawer } = useGugu();
+const sortTypeOptions = [
+	{
+		label: '不排序',
+		value: '',
+	},
+	{
+		label: '根据当前咕咕时间',
+		value: 'currentGuguLength',
+	},
+	{
+		label: '根据平均咕咕时间',
+		value: 'averageGuguLength',
+	},
+	{
+		label: '根据最大咕咕时间',
+		value: 'maxGuguLength',
+	},
+	{
+		label: '根据视频数量',
+		value: 'videoNum',
+	},
+];
 </script>
 
 <style lang="less" scoped>
